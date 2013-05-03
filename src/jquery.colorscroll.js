05be4a4dcacdc4c3419a4f63725fddf5e7e2e86f
Hendrik <lammers@gmail.com>
2013-05-03 11:40:43 +0200

    Replaced scroll event with debouncedresize plugin

--- src/jquery.colorscroll.js
+++ src/jquery.colorscroll.js
@@ -123,8 +123,11 @@
             this.options.scrollElement.on('scroll', $.proxy(this.updateColor, this));
 
             // this.maxScrollAmount changes on browser resize
-            $window.on('resize', function() {
-                // FIXME: Recalculate color positions on browser resize
+            // Use smartresize plugin: https://github.com/louisremi/jquery-smartresize
+            $window.on('debouncedresize', function() {
+                // Update new positions
+                self.setPositions();
+                self.updateColor();
             });
         },
 
@@ -132,23 +135,28 @@
             // The maximum value that can be scrolled
             this.maxScrollAmount = $document.height() - $window.height();
 
-            // Separate colors array from the options object
-            this.colors = this.options.colors;
+            // Create array to fill with converted positions
+            this.colors = new Array(this.options.colors.length);
 
             // Go through all colors
-            for (var i = 0; i < this.colors.length; i++) {
-                var pos = this.colors[i].position;
+            for (var i = 0; i < this.options.colors.length; i++) {
+                var obj = {},
+                    pos = this.options.colors[i].position;
+
+                obj.color = this.options.colors[i].color;
 
                 if (typeof pos === 'string') {
                     if (pos.charAt(pos.length - 1) === '%') {
                         // If it's a percentage convert to absolute value
-                        var per = parseFloat(pos);
-
-                        this.colors[i].position = Math.floor((per * this.maxScrollAmount) / 100);
+                        obj.position = Math.floor((parseFloat(pos) * this.maxScrollAmount) / 100);
                     } else {
-                        this.colors[i].position = parseFloat(pos);
+                        obj.position = parseFloat(pos);
                     }
+                } else {
+                    obj.position = pos;
                 }
+
+                this.colors[i] = obj;
             }
 
             // Sort array by position values
@@ -191,7 +199,7 @@
         },
 
         setColor: function (newColor) {
-            if (newColor != this.currentColor) {
+            if (newColor !== this.currentColor) {
                 this.$element.css('background-color', newColor);
                 this.currentColor = newColor;
             }
@@ -207,3 +215,51 @@
     };
 
 }(jQuery, window));
+
+/*
+ * debouncedresize: special jQuery event that happens once after a window resize
+ *
+ * latest version and complete README available on Github:
+ * https://github.com/louisremi/jquery-smartresize
+ *
+ * Copyright 2012 @louis_remi
+ * Licensed under the MIT license.
+ *
+ * This saved you an hour of work? 
+ * Send me music http://www.amazon.co.uk/wishlist/HNTU0468LQON
+ */
+(function($) {
+
+var $event = $.event,
+    $special,
+    resizeTimeout;
+
+$special = $event.special.debouncedresize = {
+    setup: function() {
+        $( this ).on( "resize", $special.handler );
+    },
+    teardown: function() {
+        $( this ).off( "resize", $special.handler );
+    },
+    handler: function( event, execAsap ) {
+        // Save the context
+        var context = this,
+            args = arguments,
+            dispatch = function() {
+                // set correct event type
+                event.type = "debouncedresize";
+                $event.dispatch.apply( context, args );
+            };
+
+        if ( resizeTimeout ) {
+            clearTimeout( resizeTimeout );
+        }
+
+        execAsap ?
+            dispatch() :
+            resizeTimeout = setTimeout( dispatch, $special.threshold );
+    },
+    threshold: 150
+};
+
+})(jQuery);
